3be1317d8f4930cd85a92c36851bdee68d0110cc
byung123 <byung14512@naver.com>
2025-02-12 19:54:45 +0900

    userControllerAdvice 수정

--- src/main/java/LacunaMatata/Lacuna/controller/user/UserControllerAdvice.java
+++ src/main/java/LacunaMatata/Lacuna/controller/user/UserControllerAdvice.java
@@ -1,7 +1,6 @@
 package LacunaMatata.Lacuna.controller.user;
 
-import LacunaMatata.Lacuna.exception.user.NotMatchPriceException;
-import LacunaMatata.Lacuna.exception.user.NotSelectItemException;
+import LacunaMatata.Lacuna.exception.user.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,5 +15,20 @@ public class UserControllerAdvice {
     @ExceptionHandler(NotMatchPriceException.class)
     public ResponseEntity<?> NotMatchPriceException(NotMatchPriceException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundUserException.class)
+    public ResponseEntity<?> NotFoundUserException(NotFoundUserException e) {
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundMyMbtiResultException.class)
+    public ResponseEntity<?> NotFoundMyMbtiResultException(NotFoundMyMbtiResultException e) {
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundMyOrderInfoException.class)
+    public ResponseEntity<?> NotFoundMyOrderInfoException(NotFoundMyOrderInfoException e) {
+        return ResponseEntity.status(404).body(e.getMessage());
     }
 }
